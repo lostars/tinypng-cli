fd54f1b99fff7f434c6cc0b489cde2172dc623ca
lostars <746320542jn@gmail.com>
2025-08-24 13:46:55 +0800

    read api key from env and command flag

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,21 @@
 package config
 
+import "os"
+
 var APIKey string
 
 type Config struct {
+}
+
+func GetAPIKey() string {
+	// read from command flag first
+	if APIKey != "" {
+		return APIKey
+	}
+	// read from env
+	key := os.Getenv("TINYPNG_API_KEY")
+	if key != "" {
+		return key
+	}
+	panic("tinypng api key not set")
 }
